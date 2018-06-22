8d6247cf47fc407e17bef1c98a4cb1365e08393e
Hans Brenna <hans.brenna@geo.uio.no>
2018-06-22 14:26:55 +0200

    Update prognostic_equations.f90

--- isobaric_dycore/prognostic_equations.f90
+++ isobaric_dycore/prognostic_equations.f90
@@ -1,3 +1,16 @@
+!Fortran subroutines for the calculation of prognostic equations forward in time
+!    Copyright (C) 2018  Hans Brenna
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!    You should have received a copy of the GNU General Public License
+!    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 subroutine prognostic_u(u_f,u_n,u_p,v_n,us,omega_n,z_n,Ps_n,omegas_n,Fl,fcor,g,a,cosphi,tanphi,phi,dlambda,dphi,dP,Pf,&
 	dt,nlambda,nphi,nP)
 !======================================================================
