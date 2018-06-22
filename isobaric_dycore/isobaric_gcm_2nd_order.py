c0642a7a9e886f4dff14454315ae91a5d5fbccfa
Hans Brenna <hans.brenna@geo.uio.no>
2018-06-22 14:22:13 +0200

    Update and rename isobaric_gcm_5x5_unif_pres_4th_order_advection_lorenz.py to isobaric_gcm_2nd_order.py

--- isobaric_dycore/isobaric_gcm_2nd_order.py
+++ isobaric_dycore/isobaric_gcm_2nd_order.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 10 10:05:48 2018
+2nd order finite difference primitive equations dynamical core with isobaric vertical coordinate
+
+    Copyright (C) 2018  Hans Brenna
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 @author: Hans Brenna
-@license: MIT
 This program implements a hydrostatic primitive equations GCM on an isobaric 
 vertical coordinate. The numerics will be 2nd order centered differences in 
 space and time on a uniform lat, lon horizontal grid. Subgrid scale diffusion 
