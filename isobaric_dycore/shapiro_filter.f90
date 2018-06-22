38a02d4de7d4407c3a7ff4a564b4439a156b7e13
Hans Brenna <hans.brenna@geo.uio.no>
2018-06-22 14:28:41 +0200

    Update shapiro_filter.f90

--- isobaric_dycore/shapiro_filter.f90
+++ isobaric_dycore/shapiro_filter.f90
@@ -1,3 +1,16 @@
+!Fortran subroutine for the calculation of the 4th order shapiro filter
+!    Copyright (C) 2018  Hans Brenna
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!    You should have received a copy of the GNU General Public License
+!    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 subroutine fourth_order_shapiro_filter(psi,vector,tau,psi_filtered,nlambda,nphi,nP)
 	!===============================================
 	!apply the 4th order shapiro filter to data. vector
