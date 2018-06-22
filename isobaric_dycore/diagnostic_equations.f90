e7c20d7af015d0df07868cf2728ae566d91f2c52
Hans Brenna <hans.brenna@geo.uio.no>
2018-06-22 14:27:47 +0200

    Update diagnostic_equations.f90

--- isobaric_dycore/diagnostic_equations.f90
+++ isobaric_dycore/diagnostic_equations.f90
@@ -1,3 +1,16 @@
+!Fortran subroutines for the calculation of diagnostic quantities
+!    Copyright (C) 2018  Hans Brenna
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!    You should have received a copy of the GNU General Public License
+!    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 SUBROUTINE diag_omega(omega_n,u_n,v_n,cosphi,a,dP,dlambda,dphi,nlambda,nphi,nP)
 	!=========================================================================
 	!Calculate the diagnostic vertical velocity $\omega$ from the divergence
